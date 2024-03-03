98c994c2d4201c4a7bb1c735bb30a9a0fe231760
Arman Ordookhani <arman.ordookhani@gmail.com>
2024-03-03 19:00:13 +0100

    Initial basic version (#1)

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # eskima
-Extract Elasticsearch/OpenSearch schema from your Go structs
+Generate Elasticsearch/OpenSearch schema from your Go structs
+
+## Basic usage
+```go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Rmaan/eskima"
+)
+
+type Book struct {
+	// 🧑‍🔧 Eskima will automatically generate schema for everything with a json tag
+	Title string `json:"title,omitempty"`
+	// 📚 All strings will be text by default, you can override type with eskima tag
+	ISBN string `json:"isbn,omitempty" eskima:"keyword"`
+	// 😌 Structs/Slices/Pointers will be followed automatically
+	Authors []Author `json:"author,omitempty"`
+}
+
+type Author struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty" eskima:"byte"`
+}
+
+func main() {
+	schema, err := eskima.Generate(Book{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buf))
+}
+
+```
+
+Output:
+```json
+{
+  "properties": {
+    "author": {
+      "properties": {
+        "age": {
+          "type": "byte"
+        },
+        "name": {
+          "type": "text"
+        }
+      }
+    },
+    "isbn": {
+      "type": "keyword"
+    },
+    "title": {
+      "type": "text"
+    }
+  }
+}
+```
+
+## Excluding fields
+To remove a field from schema, just remove it from JSON
+```go
+type Book struct {
+	InternalID int `json:"-"`
+}
+```
+
+If it's needed in JSON but not in schema, you can use alternative syntax
+```go
+type Book struct {
+	InternalID int `json:"internalID" eskima:"-"`
+}
+```
+
+To disable a field, i.e. `{"enabled": false}`, use eskima tag "!"
+```go
+type Book struct {
+	Comments []Comment `json:"comments" eskima:"!"`
+}
+```
+
+Will emit:
+```json
+{
+  "properties": {
+    "comments": {
+      "type": "object",
+      "enabled": false
+    }
+  }
+}
+```
+
+## Custom types
+You can implement `eskima.ElasticSchemaer` to have custom control over schema
+generation.
+
+For example, we can this Keyword to remove the need to specify type in eskima
+tag.
+```go
+type Keyword string
+
+func (k *Keyword) ElasticSchema() (*ElasticSchema, error) {
+	return &ElasticSchema{Type: "keyword"}, nil
+}
+
+type Book struct {
+    Publisher Keyword `json:"publisher,omitempty"`
+}
+```
---
+++ eskima.go
@@ -0,0 +1,117 @@
+package eskima
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type ElasticSchema struct {
+	Type       string                    `json:"type,omitempty"`
+	Properties map[string]*ElasticSchema `json:"properties,omitempty"`
+
+	Analyzer       string   `json:"analyzer,omitempty"`
+	Coerce         *bool    `json:"coerce,omitempty"`
+	CopyTo         []string `json:"copy_to,omitempty"`
+	DocValues      *bool    `json:"doc_values,omitempty"`
+	Enabled        *bool    `json:"enabled,omitempty"`
+	Format         string   `json:"format,omitempty"`
+	Index          *bool    `json:"index,omitempty"`
+	SearchAnalyzer string   `json:"search_analyzer,omitempty"`
+}
+
+func (s *ElasticSchema) Get(path ...string) *ElasticSchema {
+	current := s
+	for _, p := range path {
+		if current == nil {
+			return nil
+		}
+		current = current.Properties[p]
+	}
+	return current
+}
+
+type ElasticSchemaer interface {
+	ElasticSchema() (*ElasticSchema, error)
+}
+
+func Generate(t any) (*ElasticSchema, error) {
+	return generate(reflect.TypeOf(t), make([]string, 0, 10))
+}
+
+func generate(t reflect.Type, path []string) (*ElasticSchema, error) {
+	if eskimaer, ok := reflect.NewAt(t, nil).Interface().(ElasticSchemaer); ok {
+		s, err := eskimaer.ElasticSchema()
+		if err != nil {
+			return nil, fmt.Errorf("at %s: error from ElasticSchemaer: %w", strings.Join(path, "."), err)
+		}
+		return s, nil
+	}
+
+	switch t.Kind() {
+	case reflect.Struct:
+		return generateStruct(t, path)
+	case reflect.Ptr:
+		return generate(t.Elem(), path)
+	case reflect.String:
+		return &ElasticSchema{Type: "text"}, nil
+	case reflect.Int:
+		// 32 bit is a good default for int
+		return &ElasticSchema{Type: "integer"}, nil
+	case reflect.Int8:
+		return &ElasticSchema{Type: "byte"}, nil
+	case reflect.Int16:
+		return &ElasticSchema{Type: "short"}, nil
+	case reflect.Int32:
+		return &ElasticSchema{Type: "integer"}, nil
+	case reflect.Int64:
+		return &ElasticSchema{Type: "long"}, nil
+	case reflect.Uint64:
+		return &ElasticSchema{Type: "unsigned_long"}, nil
+	case reflect.Float64, reflect.Float32:
+		// TODO should they be separated? Usage of float64 for everything is commonplace but not needed in indexing.
+		return &ElasticSchema{Type: "float"}, nil
+	case reflect.Bool:
+		return &ElasticSchema{Type: "boolean"}, nil
+	case reflect.Slice:
+		return generate(t.Elem(), path)
+	default:
+		return nil, fmt.Errorf("at %s: unsupported type %s", strings.Join(path, "."), t.Kind())
+	}
+}
+
+func generateStruct(t reflect.Type, path []string) (*ElasticSchema, error) {
+	properties := map[string]*ElasticSchema{}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		jsonTag := field.Tag.Get("json")
+		if commaIndex := strings.Index(jsonTag, ","); commaIndex != -1 {
+			jsonTag = jsonTag[:commaIndex]
+		}
+		if jsonTag == "" || jsonTag == "-" {
+			continue
+		}
+
+		eskimaTag := field.Tag.Get("eskima")
+		if strings.HasPrefix(eskimaTag, "-") {
+			continue
+		}
+		if strings.HasPrefix(eskimaTag, "!") {
+			f := false
+			properties[jsonTag] = &ElasticSchema{Type: "object", Enabled: &f}
+			continue
+		}
+		if eskimaTag != "" {
+			properties[jsonTag] = &ElasticSchema{Type: eskimaTag}
+			continue
+		}
+
+		inner, err := generate(field.Type, append(path, jsonTag))
+		if err != nil {
+			return nil, err
+		}
+		properties[jsonTag] = inner
+	}
+	return &ElasticSchema{Properties: properties}, nil
+}
