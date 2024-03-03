4e5208b25882c5a70455c1a51618465c53d69937
Arman Ordookhani <arman.ordookhani@gmail.com>
2024-03-03 17:31:14 +0100

    Readme

--- fields.go
+++ fields.go
@@ -2,7 +2,7 @@ package eskima
 
 type Keyword string
 
-func (k Keyword) ElasticSchema() (*ElasticSchema, error) {
+func (k *Keyword) ElasticSchema() (*ElasticSchema, error) {
 	return &ElasticSchema{Type: "keyword"}, nil
 }
 
